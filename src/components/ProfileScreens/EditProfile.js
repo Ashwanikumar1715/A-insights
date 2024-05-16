4350d0a760837d92d90fb507d9bdeb5c1e046fe6
ashwani kumar <ashwanikumar01011997123@gmail.com>
2024-05-16 08:40:27 +0530

    img changed error

--- src/components/ProfileScreens/EditProfile.js
+++ src/components/ProfileScreens/EditProfile.js
@@ -120,7 +120,7 @@ const EditProfile = () => {
                                     <div class="absolute">
                                         Currently Image
                                     </div>
-                                    <img src={`http://localhost:5000/userPhotos/${previousPhoto}`} alt="userPhoto" />
+                                    <img src={`${baseurl}/userPhotos/${previousPhoto}`} alt="userPhoto" />
                                 </div>
 
                             </div>
